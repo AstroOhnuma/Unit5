9fb6ea461e3857ff1aeb3141e1be6ec29f66c238
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-11-13 14:54:43 -0500

    Updated from Brython Server: 11/13/2017 2:54:42 PM

--- middleword.py
+++ middleword.py
@@ -1,1 +1,9 @@
+#Astro Ohnuma
+#11/13/17
+#middleword.py - ask for a list of words and prints out the middle one
 
+words = input('Enter a list of words: ').split(' ')
+if len(words)%2 == 0:
+    print(words[len(words)/2])
+else:
+    print(words[len(words)/2])
