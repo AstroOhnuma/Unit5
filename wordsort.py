0c9b5d826995df57465ea63ffe52cbaa63ea168e
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-11-13 14:35:36 -0500

    Updated from Brython Server: 11/13/2017 2:35:35 PM

--- wordsort.py
+++ wordsort.py
@@ -1,1 +1,9 @@
+#Astro Ohnuma
+#11/13/17
+#wordsort.py - asks for a list of words, sorts them alphabetically, and then prints the list of words
 
+words = input('Enter a list of words: ').split(' ')
+words.sort()
+
+for w in words:
+    print(w)
