d96fed627948d5c3938fcb3f9cad3d70e20b141b
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-11-16 14:21:33 -0500

    Updated from Brython Server: 11/16/2017 2:21:32 PM

--- warmup13.py
+++ warmup13.py
@@ -1,1 +1,13 @@
+#Astro Ohnuma
+#11/16/17
+#warmup13.py - make a list of random numbers
+from random import randint
+numbers = []
 
+for i in range(1,20):
+    nums = randint(1,100)
+    numbers.append(nums)
+print(numbers)
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
