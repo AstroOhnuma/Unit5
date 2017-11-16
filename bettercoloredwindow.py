3345f9281d4025efc8b5abc25c1dff2e1998ca25
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-11-16 14:45:43 -0500

    Updated from Brython Server: 11/16/2017 2:45:42 PM

--- bettercoloredwindow.py
+++ bettercoloredwindow.py
@@ -1,1 +1,27 @@
+#Astro Ohnuma
+#11/16/17
+#bettercoloredwindow.py - coloredwindow.py except using a list instead of if statements
 
+from ggame import *
+from random import randint
+
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+blackoutline = LineStyle(1,black)
+    
+redbackground = RectangleAsset(600,600,blackoutline,red)
+greenbackground = RectangleAsset(600,600,blackoutline,green)
+bluebackground = RectangleAsset(600,600,blackoutline,blue)
+
+def mouseclick(event):
+    num = randint(1,3)
+    if num == 1:
+        Sprite(redbackground)
+    elif num == 2:
+        Sprite(greenbackground)
+    elif num == 3:
+        Sprite(bluebackground)
+App().listenMouseEvent('click', mouseclick)
+App().run()
