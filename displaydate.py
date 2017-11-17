baef5893d45fa9379b81c0dd3a47396ef83fa579
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-11-17 14:53:01 -0500

    Updated from Brython Server: 11/17/2017 2:53:00 PM

--- displaydate.py
+++ displaydate.py
@@ -1,5 +1,12 @@
 #Astro Ohnuma
 #11/16/17
 #displaydate.py - displays the current date
-
-
+import datetime
+today = datetime.date.today()
+today.day
+today.month
+today.year
+today.weekday()
+month = ['January','February','March','April','May','June','July','August','September','October','November','December']
+weekday = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+print('Today is',weekday[today.weekday()],',',month[today.month],today.day,today.year)
