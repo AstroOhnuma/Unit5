bf53a5ee3734bd4ef57255d8d3ec6d28e22071ba
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-11-20 14:33:28 -0500

    Updated from Brython Server: 11/20/2017 2:33:27 PM

--- antsdemo.py
+++ antsdemo.py
@@ -12,6 +12,10 @@ def step():
     for ant in data['antlist']:
         dx = randint(-4,3)
         dy = randint(-4,3)
+        if ant.x + dx > 0 and ant.x + dx < WIDTH:
+            ant.x += dx
+        if ant.y + dy > 0 and ant.y + dy < HEIGHT:
+            ant.y += dy
         ant.x += dx
         ant.y += dy
 
