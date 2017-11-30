c5c1139b5f9c8712332885f2ecef9f559fd7a63c
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-11-30 14:54:16 -0500

    Updated from Brython Server: 11/30/2017 2:54:15 PM

--- stat.py
+++ stat.py
@@ -3,11 +3,10 @@
 #stat.py - takes a list of numbers and prints out the min, max, median, mean, and mode
 
 numlist = []
+print('Type a list of numbers')
+print('Enter "q" when finished')
 
 while True:
-    numlist.append(numbers = int(input('Type a list of numbers: ')))
-    print('Enter "q" when finished')
-    print(numbers)
+    numbers = input('>').sort([' '])
     if numbers == 'q':
         break
-
