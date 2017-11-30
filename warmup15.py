8a745b9e60917d0b55cabdcfda740af9a28ed740
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-11-30 14:27:47 -0500

    Updated from Brython Server: 11/30/2017 2:27:36 PM

--- warmup15.py
+++ warmup15.py
@@ -1,1 +1,10 @@
+#Astro Ohnuma
+#11/30/17
+#warmup15.py - taking a list as an argument and returns another list where each number is doubled
 
+def doubled(A):
+    double = []
+    for i in A:
+        double.append(i*2)
+    return double
+print(doubled([2,4,6]))
