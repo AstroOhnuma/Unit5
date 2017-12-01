0e1800fcea0d4f5f9f2c430d2b1d955a0873298c
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-12-01 14:54:55 -0500

    Updated from Brython Server: 12/1/2017 2:54:53 PM

--- stat.py
+++ stat.py
@@ -7,6 +7,18 @@ print('Type a list of numbers')
 print('Enter "q" when finished')
 
 while True:
-    numbers = input('>').sort([' '])
+    numbers = input('>')
     if numbers == 'q':
         break
+    else:
+        numlist.append(int(numbers))
+print('Min: ',float(min(numlist)))
+print('Mean: ',float(sum(numlist)/len(numlist)))
+def median():
+    if len(numlist)%2 == 0:
+        print(numlist[len(numlist)/2-1],numlist[len(numlist)/2])
+    else:
+        print(numlist[len(numlist)/2])
+print('Median: ',median())
+
+
