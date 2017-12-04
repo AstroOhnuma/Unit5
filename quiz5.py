caadedec9010aaabbf330604993311c277bc4c38
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-12-04 14:26:29 -0500

    Updated from Brython Server: 12/4/2017 2:26:28 PM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,9 @@
+#Astro Ohnuma
+#12/4/17
+#quiz5.py - quiz on lists
+from random import randint
 
+def rand5():
+    randnums = [randint(1,100),randint(1,100),randint(1,100),randint(1,100),randint(1,100)]
+    
+    
