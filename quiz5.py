b8f7e4b550954954ff71218e4e2bb0a3618c91c7
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-12-04 14:49:21 -0500

    Updated from Brython Server: 12/4/2017 2:49:20 PM

--- quiz5.py
+++ quiz5.py
@@ -2,8 +2,20 @@
 #12/4/17
 #quiz5.py - quiz on lists
 from random import randint
-
+#1
 def rand5():
     randnums = [randint(1,100),randint(1,100),randint(1,100),randint(1,100),randint(1,100)]
-    
-    
+    print(randnums)
+
+rand5()
+#2
+def wordlengths(A):
+   words = []
+   for ch in words[0:]:
+       print(ch)
+print(wordlengths(['cat','dog','rat']))
+#3
+def lastelement(A):
+    print(A[len(A)-1])
+lastelement(['cat','dog','rat'])
+
